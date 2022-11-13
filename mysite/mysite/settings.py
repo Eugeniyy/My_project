1e8faafb5f8798be4204619eb4163295b56a8621
DESKTOP-KOHNKEM\Евгений <piualion@mail.ru>
2022-11-13 09:43:37 +0700

    change db to mysql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django-user',
+        'PASSWORD': 'Probe1geol',
+        'HOST': 'localhost'
     }
 }
 
